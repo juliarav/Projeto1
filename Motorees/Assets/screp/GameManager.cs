2a1b5577441ea5ad841997f66ab058e7a0255bd6
Julia Roberta Dos Anjos Vital <20221170150029@ifrn.local>
2023-11-06 12:04:00 -0300

    4

---
+++ Motorees/Assets/screp/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI hud, msgVitoria;
+    public int restantes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        restantes = FindObjectsOfType<Moeda>().Length;
+
+        hud.text = $"Moedas restantes: {restantes}";
+    }
+
+    public void SubtrairMoedas(int valor)
+    {
+        restantes -= valor;
+        hud.text = $"Moedas restantes: {restantes}";
+
+        if (restantes <= 0)
+        {
+            //ganhou o jogo
+            msgVitoria.text = "Parabéns!";
+       
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
